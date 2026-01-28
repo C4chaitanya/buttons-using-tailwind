d9531694c7b829688103eb1f36c03a57671bc97b
ChaithanyaGandham <gandham.chaithanya.ui@gmail.com>
2026-01-28 04:32:37 -0600

    created a separate page for dropdown and moved the code from App

--- src/App.js
+++ src/App.js
@@ -1,38 +1,3 @@
-import Dropdown from "./components/Dropdown";
-import { useState } from "react";
-
 export default function App() {
-  const [selectedOption, setSelectedOption] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelectedOption(option);
-  };
-
-  const dropdownOptions = [
-    {
-      id: 1,
-      label: "red",
-      name: "Red",
-    },
-    {
-      id: 2,
-      label: "green",
-      name: "Green",
-    },
-    {
-      id: 3,
-      label: "yellow",
-      name: "Yellow",
-    },
-  ];
-
-  return (
-    <div>
-      <Dropdown
-        options={dropdownOptions}
-        value={selectedOption}
-        onChange={handleSelect}
-      />
-    </div>
-  );
+  return <div>App Works!</div>;
 }
---
+++ src/pages/DropdownPage.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Dropdown from "../components/Dropdown";
+
+export default function DropdownPage() {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelectedOption(option);
+  };
+
+  const dropdownOptions = [
+    {
+      id: 1,
+      label: "red",
+      name: "Red",
+    },
+    {
+      id: 2,
+      label: "green",
+      name: "Green",
+    },
+    {
+      id: 3,
+      label: "yellow",
+      name: "Yellow",
+    },
+  ];
+
+  return (
+    <div>
+      <Dropdown
+        options={dropdownOptions}
+        value={selectedOption}
+        onChange={handleSelect}
+      />
+    </div>
+  );
+}
