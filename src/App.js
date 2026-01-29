bfbf364124b378f8b217f07c92016078e347f05a
ChaithanyaGandham <gandham.chaithanya.ui@gmail.com>
2026-01-29 06:21:41 -0600

    Added Custom Navigation and created Link component and custom hook

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import Sidebar from "./components/Sidebar";
+import RouteLinks from "./components/RouteLinks";
+
 export default function App() {
-  return <div>App Works!</div>;
+  return (
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Sidebar />
+      <RouteLinks />
+    </div>
+  );
 }
---
+++ src/components/RouteLinks.js
@@ -0,0 +1,22 @@
+import AccordionPage from "../pages/AccordionPage";
+import DropdownPage from "../pages/DropdownPage";
+import ButtonPage from "../pages/ButtonPage";
+import Route from "../components/Route";
+
+export default function RouteLinks() {
+  return (
+    <div className="col-span-5">
+      <Route path="/accordion">
+        <AccordionPage />
+      </Route>
+
+      <Route path="/">
+        <DropdownPage />
+      </Route>
+
+      <Route path="/buttons">
+        <ButtonPage />
+      </Route>
+    </div>
+  );
+}
